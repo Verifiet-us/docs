5c32f1c52fc1be9923505f4c7f9657dd7a95b9d7
Farhan Afsahi <farhan@verifiet.com>
2023-04-07 22:18:07 +0300

    New

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Verifiet Docs</span>,
- 
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: '2023 Verifiet Inc',
+    text: 'Nextra Docs Template',
   },
 }
 
