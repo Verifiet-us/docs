b1218ce8eaafaaddeb3c24e444fd063570e6f5a1
Farhan Afsahi <farhan@verifiet.com>
2023-04-07 22:10:29 +0300

    New

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,12 +3,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>Verifiet Docs</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+
 }
 
 export default config
