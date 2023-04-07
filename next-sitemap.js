de7b5a48ff402596f077cc07f287d60741f33178
Farhan Afsahi <farhan@verifiet.com>
2023-04-07 20:59:43 +0300

    New

--- next-sitemap.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://markdoc.dev',
-  generateRobotsTxt: true,
-  async additionalPaths(config) {
-    return [await config.transform(config, '/spec')];
-  }
-};
