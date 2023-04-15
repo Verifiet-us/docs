4544ae7b695cabb0afa67484e0624330e08b9868
Farhan Afsahi <farhan@verifiet.com>
2023-04-15 11:13:26 +0300

    Init

--- packages/primitives/tests/url.test.ts
+++
@@ -1,7 +0,0 @@
-import { URL } from '../url'
-
-test('URL', async () => {
-  const url = new URL('https://edge-ping.vercel.app/')
-  expect(typeof url).toBe('object')
-  expect(url.toString()).toBe('https://edge-ping.vercel.app/')
-})
