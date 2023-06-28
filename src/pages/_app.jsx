cc8bb99f4558d82b896213cf88e939d60b078227
Farhan Afsahi <91287058+Farhan-afs@users.noreply.github.com>
2023-06-28 12:21:42 +0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -29,6 +29,27 @@ export default function App({ Component, pageProps }) {
         )}
         <meta name="description" content={pageProps.description} />
       </Head>
+
+
+
+      <body>
+
+            <script>
+                window.intercomSettings = {
+                api_base: "https://api-iam.intercom.io",
+                app_id: "fghhpjgf"
+        };
+            </script>
+
+            <script>
+              
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/fghhpjgf';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+            </script>
+
+      </body>
+
+
+      
       <MDXProvider components={mdxComponents}>
         <Layout {...pageProps}>
           <Component {...pageProps} />
