81351fdbb64710efc231fdd16e30c83fbea960b9
Farhan Afsahi <91287058+Farhan-afs@users.noreply.github.com>
2023-06-30 09:04:04 +0300

    Update HeroPattern.jsx

--- src/components/HeroPattern.jsx
+++ src/components/HeroPattern.jsx
@@ -4,7 +4,6 @@ export function HeroPattern() {
   return (
     <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
       <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
-        <div className="absolute inset-0 bg-gradient-to-r from-[#0066F8] to-[#00CAFF] to-[#FDE07D] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#0066F8]/30 dark:to-[#F4722A]/30 dark:opacity-100">
           
         </div>
         
