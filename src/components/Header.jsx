42da2a5986b2b25adce09c999d2dce1e2e5c31d4
Farhan Afsahi <91287058+Farhan-afs@users.noreply.github.com>
2023-07-07 16:43:00 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -81,7 +81,7 @@ export const Header = forwardRef(function Header({ className }, ref) {
           <ModeToggle />
         </div>
         <div className="hidden min-[416px]:contents">
-          <Button href="https://dashboard.verifiet.com">Sign in</Button>
+          <Button href="https://dashboard.verifiet.com">Dashboard</Button>
         </div>
       </div>
     </motion.div>
