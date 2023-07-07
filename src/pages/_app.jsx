88c0ffa1daeb290cb6569eede4ba32c8f0e9e7d7
Farhan Afsahi <91287058+Farhan-afs@users.noreply.github.com>
2023-07-07 13:24:13 +0300

    Update _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -29,53 +29,29 @@ export default function App({ Component, pageProps }) {
         )}
         <meta name="description" content={pageProps.description} />
 
+        {/* Add the Intercom code snippet here */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/fghhpjgf'
+              (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/fghhpjgf';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+              
+              window.Intercom("boot", {
+                api_base: "https://api-iam.intercom.io",
+                app_id: "fghhpjgf"
+              });
+              
+              window.Intercom("update");
+            `,
+          }}
+        />
       </Head>
 
-      
       <MDXProvider components={mdxComponents}>
         <Layout {...pageProps}>
           <Component {...pageProps} />
         </Layout>
       </MDXProvider>
-
-      <script>
-        {`
-          window.intercomSettings = {
-            api_base: "https://api-iam.intercom.io",
-            app_id: "fghhpjgf"
-          };
-
-          (function(){
-            var w=window;
-            var ic=w.Intercom;
-            if(typeof ic==="function"){
-              ic('reattach_activator');
-              ic('update',w.intercomSettings);
-            }else{
-              var d=document;
-              var i=function(){i.c(arguments);};
-              i.q=[];
-              i.c=function(args){i.q.push(args);};
-              w.Intercom=i;
-              var l=function(){
-                var s=d.createElement('script');
-                s.type='text/javascript';
-                s.async=true;
-                s.src='https://widget.intercom.io/widget/fghhpjgf';
-                var x=d.getElementsByTagName('script')[0];
-                x.parentNode.insertBefore(s,x);
-              };
-              if(document.readyState==='complete'){
-                l();
-              }else if(w.attachEvent){
-                w.attachEvent('onload',l);
-              }else{
-                w.addEventListener('load',l,false);
-              }
-            }
-          })();
-        `}
-      </script>
     </>
   )
 }
