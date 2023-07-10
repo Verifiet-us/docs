0f6340e0044560f27b2a5693c8e8ef9d49638088
Farhan Afsahi <91287058+Farhan-afs@users.noreply.github.com>
2023-07-10 20:06:14 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,8 +43,8 @@ export const Header = forwardRef(function Header({ className }, ref) {
         !isInsideMobileNavigation &&
           'backdrop-blur-sm dark:backdrop-blur lg:left-72 xl:left-80',
         isInsideMobileNavigation
-          ? 'bg-stone-100 dark:bg-black'
-          : 'bg-stone-100/[var(--bg-opacity-light)] dark:bg-stone-950/[var(--bg-opacity-dark)]'
+          ? 'bg-white dark:bg-black'
+          : 'bg-white/[var(--bg-opacity-light)] dark:bg-black/[var(--bg-opacity-dark)]'
       )}
       style={{
         '--bg-opacity-light': bgOpacityLight,
